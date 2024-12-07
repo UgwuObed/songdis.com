504ed8c70f4e45cf982c41b0477cb3b458101ca3
UgwuObed <vjazzyvictor@gmail.com>
2024-12-07 03:38:00 +0100

    added plan component

--- app/components/apiConfig.js
+++ app/components/apiConfig.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://peaceful-waters-78164-f79460234997.herokuapp.com";
-//export const BASE_URL = "http://127.0.0.1:8000";
+//export const BASE_URL = "https://peaceful-waters-78164-f79460234997.herokuapp.com";
+export const BASE_URL = "http://127.0.0.1:8000";
