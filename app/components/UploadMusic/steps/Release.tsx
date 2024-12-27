7967e62030c0c528fc3c00d5e370f234d42bdf49
UgwuObed <vjazzyvictor@gmail.com>
2024-12-27 15:45:06 +0100

    fest

--- app/components/UploadMusic/steps/Release.tsx
+++ app/components/UploadMusic/steps/Release.tsx
@@ -21,18 +21,43 @@ const ReleaseStep = ({ uploadType, formState, setFormState }: any) => {
               value={formState.primaryGenre}
               onChange={(e) => setFormState({ ...formState, primaryGenre: e.target.value })}
             >
-              <option value="">Select Primary Genre</option>
-              <option value="Afrobeats">Afrobeats</option>
-              <option value="Reggae">Reggae</option>
-              <option value="Dancehall">Dancehall</option>
-              <option value="Pop">Pop</option>
-              <option value="Rap">Rap</option>
-              <option value="R&B">R&B</option>
-              <option value="Electronic">Electronic</option>
-              <option value="Soul">Soul</option>
-              <option value="Jazz">Jazz</option>
-              <option value="Rock">Rock</option>
-              <option value="Classical">Classical</option>
+            <option value="">Select Primary Genre</option>
+            <option value="Afrobeat">Afrobeat</option>
+            <option value="Afropop">Afropop</option>
+            <option value="Alternative">Alternative</option>
+            <option value="Big Band">Big Band</option>
+            <option value="Blues">Blues</option>
+            <option value="Children's Music">Children's Music</option>
+            <option value="Christian/Gospel">Christian/Gospel</option>
+            <option value="Classical">Classical</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Country">Country</option>
+            <option value="Dance">Dance</option>
+            <option value="Electronic">Electronic</option>
+            <option value="Fitness & Workout">Fitness & Workout</option>
+            <option value="Folk">Folk</option>
+            <option value="French Pop">French Pop</option>
+            <option value="German Folk">German Folk</option>
+            <option value="German Pop">German Pop</option>
+            <option value="Hip Hop/Rap">Hip Hop/Rap</option>
+            <option value="Holiday">Holiday</option>
+            <option value="J-Pop">J-Pop</option>
+            <option value="Jazz">Jazz</option>
+            <option value="K-Pop">K-Pop</option>
+            <option value="Latin">Latin</option>
+            <option value="Latin Urban">Latin Urban</option>
+            <option value="Metal">Metal</option>
+            <option value="New Age">New Age</option>
+            <option value="Pop">Pop</option>
+            <option value="Punk">Punk</option>
+            <option value="R&B/Soul">R&B/Soul</option>
+            <option value="Reggae">Reggae</option>
+            <option value="Rock">Rock</option>
+            <option value="Singer/Songwriter">Singer/Songwriter</option>
+            <option value="Soundtrack">Soundtrack</option>
+            <option value="Spoken Word">Spoken Word</option>
+            <option value="Vocal">Vocal</option>
+            <option value="World">World</option>
             </select>
             {/* Custom dropdown arrow */}
             <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
@@ -57,18 +82,29 @@ const ReleaseStep = ({ uploadType, formState, setFormState }: any) => {
               value={formState.secondaryGenre}
               onChange={(e) => setFormState({ ...formState, secondaryGenre: e.target.value })}
             >
-              <option value="">Select Secondary Genre</option>
-              <option value="Afrobeats">Afrobeats</option>
-              <option value="Reggae">Reggae</option>
-              <option value="Dancehall">Dancehall</option>
-              <option value="Pop">Pop</option>
-              <option value="Rap">Rap</option>
-              <option value="R&B">R&B</option>
-              <option value="Electronic">Electronic</option>
-              <option value="Soul">Soul</option>
-              <option value="Jazz">Jazz</option>
-              <option value="Rock">Rock</option>
-              <option value="Classical">Classical</option>
+        <option value="">Select Secondary Genre</option>
+        <option value="Afrofusion">Afrofusion</option>
+        <option value="Amapiano">Amapiano</option>
+        <option value="Highlife">Highlife</option>
+        <option value="Soukous">Soukous</option>
+        <option value="Mbalax">Mbalax</option>
+        <option value="Kwaito">Kwaito</option>
+        <option value="Gnawa">Gnawa</option>
+        <option value="Taarab">Taarab</option>
+        <option value="Hip-Hop/Rap">Hip-Hop/Rap</option>
+        <option value="R&B">R&B</option>
+        <option value="Electronic">Electronic</option>
+        <option value="Pop">Pop</option>
+        <option value="Afrobeats">Afrobeats</option>
+        <option value="Latin">Latin</option>
+        <option value="Reggae">Reggae</option>
+        <option value="Dancehall">Dancehall</option>
+        <option value="Country">Country</option>
+        <option value="Rock">Rock</option>
+        <option value="Jazz">Jazz</option>
+        <option value="Classical">Classical</option>
+        <option value="World">World</option>
+        <option value="Podcast">Podcast</option>
             </select>
             {/* Custom dropdown arrow */}
             <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
@@ -99,7 +135,7 @@ const ReleaseStep = ({ uploadType, formState, setFormState }: any) => {
 
         <div className="space-y-2">
           <label className="block text-sm font-medium text-gray-700">
-            Pre-Order Date
+            Pre-Order Date(Optional)
           </label>
           <input
             type="date"
